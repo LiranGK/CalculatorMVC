cb54b085a11efcfd89cf9104c40196de1a222c53
helarye <helarye@gmail.com>
2025-09-10 08:58:10 +0300

    Add project files.

---
+++ CalculatorSess0809/Models/Calculator.cs
@@ -0,0 +1,9 @@
+﻿namespace CalculatorSess0809.Models
+{
+    public class Calculator
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+        public int Result { get; set; }
+    }
+}
---
+++ CalculatorSess0809/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CalculatorSess0809.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
