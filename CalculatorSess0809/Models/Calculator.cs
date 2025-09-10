a1ae2ffd66d63064b91e16226970b929b5bc74e4
LiranGK <lirangilday@gmail.com>
2025-09-10 13:51:07 +0300

    Add files via upload

---
+++ CalculatorSess0809/Models/Calculator.cs
@@ -0,0 +1,12 @@
+﻿namespace CalculatorSess0809.Models
+{
+    public class Calculator
+    {
+        public float Number1 { get; set; }
+        public float Number2 { get; set; }
+        public float Result { get; set; }
+        public float CurrResult { get; set; }
+        public string Equation { get; set; }
+        public string TempEquation { get; set; }
+    }
+}
